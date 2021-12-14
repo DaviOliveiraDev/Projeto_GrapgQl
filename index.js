be15cf482f1b5f9dda8af0c8e3bc324a4e636a98
Davi de Oliveira <daviolirsoares@gmail.com>
2021-12-13 22:48:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const typeDefs = gql`
+const typeDefs = gql`   
+    scalar Date
 
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        precoComDesconto: Float
+    }
+
+    # Pontos de entrada da sua API
+    type Query {
+        ola: String!
+        hora_atual: Date!
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+    }
 `
 
 const resolvers = {
+    Produto: {
+        precoComDesconto(produto) {
+            if (produto.desconto) {
+               return produto.preco - (produto.preco * produto.desconto)
+            } else {
+                return produto.preco
+            }
+        }
+    },
 
+    Usuario: {
+        salario(props) {
+            return props.salario_real
+        }
+    },
+
+    Query: {
+        ola () {
+            return 'Bom dia'
+        },
+        hora_atual () {
+            // var today = new Date();
+            //return today.getFullYear() + '-' + today.getMonth() + '-' + today.getDay()
+            return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Davi',
+                email: 'email@gmail.com',
+                idade: 12,
+                salario_real: 131.12,
+                vip: true,
+        }},
+        
+        produtoEmDestaque() {
+            return {
+                nome: 'Pastel',
+                preco: 100,
+                desconto: 0.5, 
+            }
+        }
+    }
 }
 
 const server = new ApolloServer({
